c89160a6e072bb1d7ddf71822dc5d0d92ea3999d
Aishwarya Kulkarni <aishwaryackulkarni@gmail.com>
2021-11-10 22:21:25 -0800

    Update heapmodule.py
    Part A- 5 steps completed

--- heapmodule.py
+++ heapmodule.py
@@ -37,7 +37,6 @@ heap = {}
 
 heap_count = 0  # how many objects stored in the heap
 
-# 1.
 activation_stack = []  # This is the handle to the namespace in the heap that holds the
          # program's global variables.  See  initializeHeap  below.
 
@@ -47,6 +46,10 @@ activation_stack = []  # This is the handle to the namespace in the heap that ho
 def isEmpty():
   """check whether activation stack is empty or not"""
   global activation_stack
+  if len(activation_stack) == 0:
+        return True
+  else:
+        return False
   # WRITE ME
 
 
@@ -54,30 +57,29 @@ def pushHandle(handle):
   """ push handle on the activation_stack"""
   global activation_stack
   # WRITE ME
-  activation_stack.append(...)
+  activation_stack.append(handle)
 
 def popHandle():
   """ pop a handle from stack """
   global activation_stack
   # if  activation state is not empty 
   # WRITE ME
-  if not isEmpty:
-     ....
+  if not isEmpty():
+    return activation_stack.pop()
+     
 
 def topHandle():
   """ return the top handel on the activation_stack"""
   global activation_stack
   # WRITE ME
-
+  if not isEmpty():
+    return activation_stack[-1]
 
 def activeNS():
     """returns the handle of the namespace that holds the currently visible
        program variables
     """
-    # WRITE ME write this function
-
-    pass
-    #return top handle on the activation stack
+    return topHandle()
 
 def declare(handle, field, rval) :
     """creates a new definition in the heap at (handle, field) and initializes
@@ -88,34 +90,39 @@ def declare(handle, field, rval) :
                rval -- an int or a handle
     """
     ## WRITE ME: # int x = 1 ["int", "x" ,"1"] heap[handle][field] = rval #heap = {handle:{field:rval}}
+    if field not in heap[handle]:    
+        heap[handle][field] = rval
+    else:
+        crash("redeclaration error")
     pass
-    
+
+
 def initializeHeap():
     """resets the heap for a new program"""
     global heap_count, heap, activation_stack
-    handle = allocateNS()  # create namespace in  heap  for global variables heap={'h0':{}}
-    heap[handle]['parentns'] = 'nil'
-    # WRITE ME
-    pushHandle(...)
+    handle = allocateNS()  # create namespace in  heap  for global variables
+    heap[handle]["parentns"]="nil"
+    pushHandle(handle)
     
 
 
 def printHeap(): 
     """prints contents of  ns  and  heap"""
-    print("activation stack =", activation_stack)
+    print("activation_stack =", activation_stack)
 
     print("heap = {")
     handles = sorted(heap.keys())
+    # handles.sort()
     for h in handles: 
         print(" ", h, ":", heap[h])
     print("}")
-    
+
 
 def allocateNS() :
     """allocates a new, empty namespace in the heap and returns its handle"""
     global heap_count
     newloc = "h" + str(heap_count)  # generate handle of form,  hn,  where  n  is an int
-    heap[newloc] = {} # heap = {....'h4': {}}
+    heap[newloc] = {}
     heap_count = heap_count + 1
     return newloc
 
@@ -140,6 +147,8 @@ def lookup(handle, field) :
         crash("invalid lookup address: " + handle + " " + field)
 
 
+
+
 def update(handle, field, rval) :
     """updates  rval  at heap[handle][field], provided that
          (i)  isLValid(handle,field)
@@ -152,7 +161,6 @@ def update(handle, field, rval) :
     """
     ## REVISE THE FOLLOWING CODE TO MATCH THE ABOVE DOCUMENTATION:
     heap[handle][field] = rval
-
 
 def crash(message) :
     """prints message and stops execution"""
